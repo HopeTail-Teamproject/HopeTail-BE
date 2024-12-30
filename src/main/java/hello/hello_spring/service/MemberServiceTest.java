77ac8160e2f725796fb722c9b0af0656d43afb12
kyoohyunlee <khyhlee@naver.com>
2024-12-31 03:23:22 +0900

    dev: membership program ing

---
+++ src/main/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,12 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemberServiceTest {
+    
+}
