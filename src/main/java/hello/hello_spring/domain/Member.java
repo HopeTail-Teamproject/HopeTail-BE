63f153d144be67c41306c32f3c254f425cae6582
kyoohyunlee <khyhlee@naver.com>
2025-03-18 19:09:44 +0900

    Member테이블 수정

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -1,8 +1,6 @@
 package hello.hello_spring.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,13 +10,25 @@ import lombok.Setter;
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Increment 적용
     private Long id;
 
-    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
 
-    private Integer age;
-
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private String profileImage;
+
+    private String bio;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts; // 한 사용자가 여러 게시글 작성 가능
+
 
 
 }
