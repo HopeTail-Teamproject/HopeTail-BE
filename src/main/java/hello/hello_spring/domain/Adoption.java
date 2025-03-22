ce6a051a342a333aee163a19f313a82b6f176930
kyoohyunlee <khyhlee@naver.com>
2025-03-22 19:54:19 +0900

    adoption 테이블 생성

---
+++ src/main/java/hello/hello_spring/domain/Adoption.java
@@ -0,0 +1,38 @@
+package hello.hello_spring.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Adoption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 입양 신청한 유기견
+    @ManyToOne
+    @JoinColumn(name = "pet_id", nullable = false)
+    private Pet pet;
+
+    // 신청자
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.REQUESTED;
+
+    private LocalDateTime appliedAt = LocalDateTime.now();
+
+    public enum Status {
+        REQUESTED, APPROVED, REJECTED
+    }
+}
