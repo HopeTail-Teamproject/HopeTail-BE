cbc337d8fb65b0304f644be26bd6f906ea790408
kyoohyunlee <khyhlee@naver.com>
2025-03-22 19:53:44 +0900

    comment 테이블 수정

--- src/main/java/hello/hello_spring/domain/Comment.java
+++ src/main/java/hello/hello_spring/domain/Comment.java
@@ -2,25 +2,33 @@ package hello.hello_spring.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
-@Getter
+import java.time.LocalDateTime;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 게시글
     @ManyToOne
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
+    // 작성자
     @ManyToOne
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
