b7971158a1867777cd72870599e00750cdddae8e
DESKTOP-GO177QK\박진우 <jinwoo2202@gmail.com>
2025-03-22 15:39:58 +0900

    로그인, 회원가입, 인증 db 추가

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -3,7 +3,11 @@ package hello.hello_spring.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Setter
@@ -24,9 +28,37 @@ public class Member {
     @Column(nullable = false)
     private String passwordHash;
 
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @CreationTimestamp
+    private LocalDateTime createdat;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedat;
+
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("PENDING")
+    private Status status;
+
+    @Column
+    private LocalDateTime lastLogin;
+
+    @ColumnDefault("0")
+    private boolean emailCertified;
+
+    @ColumnDefault("0")
+    private boolean phoneCertified;
+
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts; // 한 사용자가 여러 게시글 작성 가능
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Token> Tokens; // 한 사용자가 여러 토큰 가질 수 있음
+}
 
-
+enum Status {
+    ACTIVE,
+    INACTIVE,
+    PENDING                  // 인증 받기 전 PENDING 으로 대기 상태 유지
 }
---
+++ src/main/java/hello/hello_spring/domain/Token.java
@@ -0,0 +1,56 @@
+package hello.hello_spring.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String Token;
+
+    @CreationTimestamp
+    private LocalDateTime createdat;
+
+    private LocalDateTime expiredat;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+
+    @Enumerated(EnumType.STRING)
+    private CertificationType certificationType;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Member member;
+
+}
+
+enum Type {
+    ACCESS,
+    REFRESH,
+    CERTIFICATION,
+    PASSWORD_RESET,
+    FIND_ID
+}
+
+enum CertificationType {
+    EMAIL,
+    PHONE
+}
