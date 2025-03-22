c0d23444c67ed7942b93ae577a70c62444745923
DESKTOP-GO177QK\박진우 <jinwoo2202@gmail.com>
2025-03-23 00:40:28 +0900

    자바 버전 픽스 및 env 파일 추가(db 연동시 개별적인 연동 정보 관리)

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -1,12 +1,16 @@
 package hello.hello_spring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloSpringApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
