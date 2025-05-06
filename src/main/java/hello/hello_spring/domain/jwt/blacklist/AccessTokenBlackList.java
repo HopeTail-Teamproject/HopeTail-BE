f9419043c704fdcef74dd192418bb9b76164b50f
DESKTOP-GO177QK\jinwo <jinwoo2202@naver.com>
2025-05-06 22:42:16 +0900

    진짜 마지막수정

--- src/main/java/hello/hello_spring/domain/jwt/blacklist/AccessTokenBlackList.java
+++ src/main/java/hello/hello_spring/domain/jwt/blacklist/AccessTokenBlackList.java
@@ -1,9 +1,8 @@
 package hello.hello_spring.domain.jwt.blacklist;
 
+import hello.hello_spring.domain.jwt.JwtProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,19 +10,15 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class AccessTokenBlackList {
 
     private final RedisTemplate<String, Object> redisBlackListTemplate;
 
-    public AccessTokenBlackList(@Qualifier("redisTemplate") RedisTemplate<String, Object> redisBlackListTemplate) {
-        this.redisBlackListTemplate = redisBlackListTemplate;
-    }
-
-    @Value("${jwt.access-token-validity-in-seconds}")
-    private Long accessTokenTimeoutInSeconds;
+    private final JwtProperties jwtProperties;
 
     public void setBlackList(String key, Object o) {
-        redisBlackListTemplate.opsForValue().set(key, o, accessTokenTimeoutInSeconds, TimeUnit.SECONDS);
+        redisBlackListTemplate.opsForValue().set(key, o, jwtProperties.getAccessTokenValidityInSeconds(), TimeUnit.SECONDS);
     }
 
     public Object getBlackList(String key) {
