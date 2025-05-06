42d4f8101179ff314b84d03f87afcba48ec2fe0c
DESKTOP-GO177QK\jinwo <jinwoo2202@naver.com>
2025-05-06 20:42:08 +0900

    env 관련 수정

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -10,7 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+				.directory(System.getProperty("user.dir"))// 또는 상대경로: "../"
+				.filename(".env")
+				.load();
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
