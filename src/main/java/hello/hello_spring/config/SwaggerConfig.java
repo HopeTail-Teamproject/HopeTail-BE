9fc598b3d5b4401710302cd495bc8c1182a86022
DESKTOP-GO177QK\jinwo <jinwoo2202@naver.com>
2025-05-09 23:15:28 +0900

    최종 수정

--- src/main/java/hello/hello_spring/config/SwaggerConfig.java
+++ src/main/java/hello/hello_spring/config/SwaggerConfig.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 /**
  * Swagger springdoc-ui 구성 파일
@@ -14,14 +16,24 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
         Info info = new Info()
                 .title("API Document")
                 .version("v0.0.1")
                 .description("API 명세서입니다.");
 
         return new OpenAPI()
-                .components(new Components())
-                .addServersItem(new Server().url("/")) // 추가
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().
+                        addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+
+                )).addServersItem(new Server().url("/")) // 추가
                 .info(info);
+
     }
 }
