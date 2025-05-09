aa547bea4620b7baf9f2462e057e4d311370db7d
minsung-phy <minsung.phy@gamil.com>
2025-05-09 14:23:38 +0900

    dev : 게시글 이메일로 찾기 컨트롤러, 서비스

--- src/main/java/hello/hello_spring/controller/PostController.java
+++ src/main/java/hello/hello_spring/controller/PostController.java
@@ -69,16 +69,16 @@ public class PostController {
     }
 
     @Operation(summary = "이메일로 게시글 찾기")
-    @GetMapping("/{postEmail}")
+    @GetMapping("/posts/by-email")
     public List<PostResponseDto> getPostsByEmail(@RequestParam String email){
         return postService.getPostsByEmail(email);
     }
 
-    @Operation(summary = "로그인한 유저 게시글 찾기")
-    @GetMapping("/{memebersPosts}")
-    public List<PostResponseDto> getPostsByLoginEmail(@RequestParam HttpServletRequest request){
-        return postService.getPostsByLoginEmail(request);
-    }
+//    @Operation(summary = "로그인한 유저 게시글 찾기")
+//    @GetMapping("/posts/by-login")
+//    public List<PostResponseDto> getPostsByLoginEmail(@RequestParam HttpServletRequest request){
+//        return postService.getPostsByLoginEmail(request);
+//    }
 }
 
 
