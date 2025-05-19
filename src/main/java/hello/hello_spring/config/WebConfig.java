1cf1be22e27f4f396530074de65eb4e378fa670a
DESKTOP-GO177QK\jinwo <jinwoo2202@naver.com>
2025-05-19 20:03:38 +0900

    cors 수정

--- src/main/java/hello/hello_spring/config/WebConfig.java
+++ src/main/java/hello/hello_spring/config/WebConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
+        registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:8080/",   // 로컬 테스트
+                        "http://localhost:8080",   // 로컬 테스트
                         "https://hopetail-teamproject.github.io/",  // GitHub Pages
                         "https://api.hopetail.com/"             // API 서버 주소 (HTTPS)
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("")                         // 모든 헤더 허용
+                .allowedHeaders("*")                         // 모든 헤더 허용
                 .allowCredentials(true);                     // 쿠키 허용
     }
 }
