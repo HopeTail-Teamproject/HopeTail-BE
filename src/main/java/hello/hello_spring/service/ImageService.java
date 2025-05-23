11fbeefd2746febb2cb5643049ae05877d4d8eaf
kyoohyunlee <khyhlee@naver.com>
2025-05-23 20:01:18 +0900

    image/경로  -> images/경로 바꿈

--- src/main/java/hello/hello_spring/service/ImageService.java
+++ src/main/java/hello/hello_spring/service/ImageService.java
@@ -25,6 +25,7 @@ public class ImageService {
         Path savedPath = imageDir.resolve(savedFileName);
         image.transferTo(savedPath.toFile()); //실제 이미지 파일 저장
 
+        //images로 찾아가게
         return "/images/" + savedFileName; //저장한 이미지 조회할 수 있는 url WebConfig에서 매핑해줌
     }
 
